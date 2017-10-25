34a54440c8f7a8b967b5e6c671d42a3cc867256c
Такаев Юрий <takaev@d-systems.local>
2017-10-25 17:23:01 +0300

    add psycopg2

--- unicom24project/settings.py
+++ unicom24project/settings.py
@@ -126,7 +126,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 import dj_database_url
 
-db_from_env =dj_database_url.config()
+db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
 # Сообщает о том, на какой URL-адрес перенаправлять пользователя после входа в систему.
