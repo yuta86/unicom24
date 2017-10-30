d74d4ecbc2a5cc54f0accdb03701e658de5c74ec
Такаев Юрий <takaev@d-systems.local>
2017-10-30 11:42:44 +0300

    error correction DB

--- unicom24project/settings.py
+++ unicom24project/settings.py
@@ -26,6 +26,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['myunicom24.herokuapp.com', 'localhost', '127.0.0.1']
 
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -76,10 +77,10 @@ WSGI_APPLICATION = 'unicom24project.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 # DATABASES = {
-# 'default': {
-# 'ENGINE': 'django.db.backends.sqlite3',
-# 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-# }
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
 # }
 
 DATABASES = {
@@ -140,9 +141,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Update database configuration with $DATABASE_URL.
 import dj_database_url
-
-db_from_env = dj_database_url.config()
+db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 # Сообщает о том, на какой URL-адрес перенаправлять пользователя после входа в систему.
