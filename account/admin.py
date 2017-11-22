e120297f6ff7cb1cf8061eae7878c99dfb69e273
yuta <yuta_86@mail.ru>
2017-11-22 13:09:58 +0300

    some feature

--- account/admin.py
+++ account/admin.py
@@ -2,16 +2,36 @@ from django.contrib import admin
 from .models import Profile, Offer, Partner, Request
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
+from django.db import models
+from django.forms import widgets
 
 
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ['user', 'date_of_birth', 'photo', 'sex', 'city', 'passport', 'phone', 'score',
-                    'social_status','group']  # вывод указанных полей
+                    'social_status', 'group']  # вывод указанных полей
     list_editable = ['date_of_birth', 'photo', 'sex', 'city', 'passport', 'phone', 'score',
                      'social_status', 'group']  # поля для редактирования в Админке
     list_filter = ['city', 'score', 'social_status', 'group']  # фильтр по столбцу
     search_fields = ['city', 'passport', 'phone', 'user']  # поиск по столбцам
-    #ordering = ['user'] # сортировка
+
+    # list_display_links = ['city']  # ссылочные поля
+
+    # ordering = ['user'] # сортировка
+    # fields = ['city'] отображение полей
+    # exclude = ['city'] неотображаемые поля
+    # readonly_fields = ['city'] поля для чтения
+    # fieldsets = [   # разделение формы на две и более частей
+    #     (None,
+    #      {'fields': ['user', 'date_of_birth', 'city', 'photo', 'phone'] }),
+    #     ('Data information', {'fields': ['group','passport', 'score', 'sex', 'social_status'],
+    #                           'classes': ['collapse']})
+    # ]
+
+    # переопределение виджета DateField
+    # formfield_overrides = {
+    #     models.DateField: {'widget': widgets.TextInput}
+    # }
+
 
 admin.site.register(Profile, ProfileAdmin)
 
@@ -48,6 +68,9 @@ class OfferAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
     search_fields = ['name', 'email']  # поиск по столбцам
 
+    save_as = True  # для появления кнопки "Сохранить как"
+    save_on_top = True  # дополнительные кнопки сверху
+
 
 admin.site.register(Offer, OfferAdmin)
 
@@ -57,6 +80,7 @@ class RequestAdmin(admin.ModelAdmin):
     list_filter = ['created', 'offer', 'type_status']  # фильтр по столбцу
     list_editable = ['type_status']  # поля для редактирования в Админке
     search_fields = ['created', 'offer', 'type_status']  # поиск по столбцам
+    # raw_id_fields = ['offer'] создание доп. окна, как выбор из списка
 
 
 admin.site.register(Request, RequestAdmin)
